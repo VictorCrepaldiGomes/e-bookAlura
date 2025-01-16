0d4ccf6079791489e02a1fd423b6a836a9beab06
VictorGomes <victorcrepaldigomes@gmail.com>
2025-01-16 15:01:41 -0300

    envio novo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
+import Logo from './componentes/Logo/Logo';
+import perfil from './imagens/perfil.svg';
+import sacola from './imagens/sacola.svg';
+
+
+const textoOpcoes = ['Categorias', 'Minha Estante', 'Favoritos'];
+
+const icones = [perfil, sacola]
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+        <ul className="opcoes">
+          {textoOpcoes.map((texto) => (
+            <li className='opcao'><p>{texto}</p></li>
 
+          ) )}
+        </ul>
+
+          <ul className='icones'>
+            {icones.map((icone) => (
+              <li className='iconezinho'><img src={icone}></img></li>
+          ) )}
+
+          </ul>
+        
       </header>
-
     </div>
   );
-
 }
 
 export default App;
---
+++ src/componentes/Logo/Logo.js
@@ -0,0 +1,21 @@
+import './Logo.css';
+import logo from '../../imagens/logo.svg';
+
+const Logo = () => {
+    
+    return (
+        <div className='logo'>
+        <img src={logo} 
+        alt='logo' 
+        className='ajuste'
+        ></img>
+        <p><strong>Alura</strong>Books</p>
+        </div>
+    )
+}
+
+
+
+
+
+export default Logo
