8e4d59ed963748207d2f687c91a090b4f22c813d
VictorGomes <victorcrepaldigomes@gmail.com>
2025-02-05 10:29:02 -0300

    Rota favoritos funcionando

--- src/OpcoesHeader/Opcoes.js
+++ src/OpcoesHeader/Opcoes.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './Opcoes.css';
 
-const textoOpcoes = ['Categorias', 'Minha Estante', 'Favoritos'];
+const textoOpcoes = [
+  { texto: 'Categorias', path: '/categorias' },
+  { texto: 'Minha estante', path: '/estante' },
+  { texto: 'Favoritos', path: '/favoritos' }
+];
 
 function OpcoesHeader() {
     return (
         <ul className="opcoes">
-        {textoOpcoes.map((texto) => (
-          <li className='opcao'><p>{texto}</p></li>
-          
+        {textoOpcoes.map((opcao) => (
+          <li className='opcao' key={opcao.texto}>
+            <Link to={opcao.path}>
+              <p>{opcao.texto}</p>
+            </Link>
+          </li>
+           
 
         ))}
         </ul>
